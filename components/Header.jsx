6815c3d06cc056dcaa92c1732e89d5cffb7376a1
polcuenca <115073490+polcuenca@users.noreply.github.com>
2024-07-11 16:45:51 +0800

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header(){
     return(
         <header className="header">
-            //<img className="header--image" src="./images/Subtract.png" alt="logo.png" />
+            <img className="header--image" src="./images/Subtract.png" alt="logo.png" />
             <h1 className="header--title">Meme Generator</h1>
             <h3 className="header--project">Scrimba Project by Paul</h3>
         </header>   
